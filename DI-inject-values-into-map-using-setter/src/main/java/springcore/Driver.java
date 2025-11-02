64a69e801e62b3b0004fbd9258e8204bcd602f3e
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-11-03 00:15:28 +0530

    DI inject values into map using setter

---
+++ DI-inject-values-into-map-using-setter/src/main/java/springcore/Driver.java
@@ -0,0 +1,13 @@
+package springcore;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+	public static void main(String[] args) {
+		BeanFactory factory = new ClassPathXmlApplicationContext("myConfig.xml");
+		Student s = (Student)factory.getBean("myStudent");
+		System.out.println(s);
+		
+	}
+}
---
+++ DI-inject-values-into-map-using-setter/src/main/java/springcore/Student.java
@@ -0,0 +1,34 @@
+package springcore;
+
+import java.util.List;
+
+public class Student {
+	private int id;
+	private String name;
+	private List<String> courses;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getCourses() {
+		return courses;
+	}
+	public void setCourses(List<String> courses) {
+		this.courses = courses;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", courses=" + courses + "]";
+	}
+	
+	
+	
+}
