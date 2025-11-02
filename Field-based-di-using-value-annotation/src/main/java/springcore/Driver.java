4aeb50cf74ffe9c1d87b6a0f0cd5211a979d007d
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-11-03 00:22:10 +0530

    Field based DI using value annotation

---
+++ Field-based-di-using-value-annotation/src/main/java/springcore/Driver.java
@@ -0,0 +1,13 @@
+package springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Driver {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(myConfig.class);
+		Account ac = context.getBean(Account.class);
+		System.out.println(ac);
+		
+	}
+}
---
+++ Field-based-di-using-value-annotation/src/main/java/springcore/myConfig.java
@@ -0,0 +1,13 @@
+package springcore;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "springcore")
+public class myConfig {
+	
+	
+	
+	
+}
