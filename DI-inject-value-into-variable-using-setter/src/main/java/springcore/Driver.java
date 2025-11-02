ce87c2b137df54d2c87f52e478745e1fe26a7433
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-11-03 00:13:34 +0530

    DI inject value into variable using setter

---
+++ DI-inject-value-into-variable-using-setter/src/main/java/springcore/Driver.java
@@ -0,0 +1,13 @@
+package springcore;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+	public static void main(String[] args) {
+		BeanFactory factory = new ClassPathXmlApplicationContext("myConfig.xml");
+		Student s = factory.getBean("myStudent", Student.class);
+		System.out.println(s);
+		
+	}
+}
